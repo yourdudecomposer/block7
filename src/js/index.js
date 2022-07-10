b5a27249202c8afdd5a10dc33c144ee88f009868
Filipp Goriainov <barrios1991phil@gmail.com>
2022-07-10 10:35:35 +0400

    add swiper

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 import '../scss/style.scss';
 import './page-nav.js';
-import './intro-article.js'
+import './intro-article.js';
+import './swiper.js'
 
 
 
---
+++ src/js/swiper.js
@@ -0,0 +1,19 @@
+import Swiper, { Navigation, Pagination } from 'swiper';
+
+window.onload = () => {  // ожидаем загрузку окна браузера
+    if (window.matchMedia('(max-width: 767px)').matches) { // // свайпер у нас будет работать, если разрешение эерана не превышает 767px
+        const swiper = new Swiper('.swiper', {
+            // инициализируем новый Swiper
+            modules: [Navigation, Pagination],
+            direction: 'horizontal', // устанавливаем напрвление Swiper
+            loop: true,  // делаем Swiper зацикленным (Swiper самостоятельно добавит слайды в начало и конец .swiper-wrapper для создания иллюзии "бесконечности" слайдов)
+            spaceBetween: 20, // отступ между слайдами в px
+            slidesPerView: 'auto', // позволит устанавливать произвольную ширину слайдов, в противном случае - растянет на ширину контейнера .swiper-wrapper
+            pagination: { // подключаем пагинацию
+                el: '.swiper-pagination', // контейнер для пагинации
+                clickable: true // добавляем параметр, если хотим сделать bullets кликабельными
+            },
+            init: true
+        })
+    }
+}
